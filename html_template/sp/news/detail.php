909bf7f96e50ad8d9365a3f7ad6bfd44d304687b
Ippei Takahashi <takahashi@6web.co.jp>
2015-06-02 13:19:47 +0900

    フロント：お知らせ(refs #192) -お知らせ実装

---
+++ html_template/sp/news/detail.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo $news_data['title'];?>｜ポイント.com</title>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<?php include_once dirname(__FILE__).'/../common/header_meta.php';?>
+</head>
+<body id="register">
+<!--全体-->
+<div id="wrap">
+<a name="top" id="top"></a>
+
+
+<!--ヘッダ-->
+<?php include_once dirname(__FILE__).'/../common/header_contents.php';?>
+<!--ヘッダ-->
+<!--メイン全体-->
+<div id="mainbodywrap">
+<!--ページメイン部分-->
+<div id="mainbody" class="clearfix">
+<!--コンテンツ-->
+<div class="contents">
+	<h2>POINT.COMからのお知らせ</h2>
+	<h3><?php echo $news_data['title'];?></h3>
+	
+	<?php if($images):?>
+	<!-- start:スライド写真-->
+	<div class="photoslide">
+		<?php foreach($images as $img):?>
+		<div>
+		<p><img src="/files/images/<?php echo $img;?>" alt=""/></p>
+		</div>
+		<?php endforeach;?>
+	</div>
+	<!-- end:スライド写真 -->
+	<?php endif;?>
+	
+	<p><?php echo $news_data['body'];?></p>
+	<p><a href="/news/" class="linkbtn block alncenter">一覧へ戻る</a></p>
+
+</div>
+<!--/コンテンツ-->
+
+<div id="footer">
+<address>
+Copyright 2015 POINT.COM All Rights Reserved
+</address>
+</div>
+
+</div>
+<!--/ページメイン部分-->
+</div>
+<!--/メイン全体-->
+
+
+</div>
+
+<!--/全体-->
+
+<!--スライド-->
+<?php include_once dirname(__FILE__).'/../common/slide_contents.php';?>
+<!-- /スライド-->
+
+
+</body>
+</html>
--- system/model/database/NewsDbModel.php
+++ system/model/database/NewsDbModel.php
@@ -22,4 +22,46 @@ class NewsDbModel extends DbModel{
 			'delete_flg'
 		);
 	}
+	
+	/**
+	 * 一覧用データ取得
+	 * 
+	 * @param int $start_page 開始番号
+	 * @param int $get_page 取得する件数
+	 * @return array 詳細データ複数件
+	 */
+	public function getNewsList($start_page,$get_page){
+		$sql = "SELECT news_id,display_date,title,image1,image2,image3 FROM {$this->table} WHERE ";
+		$sql.= "delete_flg = 0 AND ";
+		$sql.= "public = 1 AND ";
+		$sql.= "public_start_date < NOW() AND ";
+		$sql.= "(public_end_date > NOW() OR public_end_date IS NULL) ";
+		$sql.= " ORDER BY public_start_date DESC ";
+		$sql.= "LIMIT {$start_page},{$get_page}";
+		return $this->db->getAllData($sql);
+	}
+	
+	
+	
+	/**
+	 * 詳細データ取得
+	 * 
+	 * @param int $id 主キー
+	 * @return array 詳細データ1件
+	 */
+	public function getNewData($id){
+		
+		if(!is_numeric($id)){
+			return NULL;
+		}
+		
+		$field = $this->getFieldText();
+		$sql = "SELECT {$field} FROM {$this->table} WHERE ";
+		$sql.= "news_id = '{$id}' AND ";
+		$sql.= "delete_flg = 0 AND ";
+		$sql.= "public = 1 AND ";
+		$sql.= "public_start_date < NOW() AND ";
+		$sql.= "(public_end_date > NOW() OR public_end_date IS NULL) ";
+		return $this->db->getData($sql);
+	}
 }
