16709f77dce35c8b41e2efd4d1e9b411b8b3c5c4
Ippei Takahashi <takahashi@6web.co.jp>
2015-06-09 18:42:41 +0900

    フロント：ポイントコード(refs #196) -実装

---
+++ html_template/sp/mypage/point_code/none.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>ポイントコード表示（確認）｜ポイント.com</title>
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<?php include_once dirname(__FILE__).'/../../common/header_meta.php';?>
+
+		<!--[if lte IE 9]>
+		<script src="/js/html5.js"></script>
+		<script src="/js/css3-mediaqueries.js"></script>
+		<![endif]-->
+
+
+		<script type="text/javascript" src="/js/jquery.js"></script>
+		<script type="text/javascript" src="/js/obj.js"></script>
+		<script type="text/javascript" src="/js/font-size.js"></script>
+		<script type="text/javascript" src="/js/smooth.pack.js"></script>
+		<script type="text/javascript" src="/js/acc.js"></script>
+
+		<!--スライドメニュー-->
+		<script src="/js/sidr/jquery.sidr.min.js"></script>
+		<link rel="stylesheet" href="../js/sidr/jquery.sidr.light.css">
+
+	</head>
+	<body id="register">
+		<!--全体-->
+		<div id="wrap">
+			<a name="top" id="top"></a>
+			<!--ヘッダ-->
+			<?php include_once dirname(__FILE__).'/../../common/header_contents.php';?>
+			
+			<!--ヘッダ-->
+				<!--メイン全体-->
+					<div id="mainbodywrap">
+						<!--ページメイン部分-->
+						<div id="mainbody" class="clearfix">
+						<!--コンテンツ-->
+						<div class="contents">
+								<h2>ポイントコード表示（確認）</h2>
+								<p>現在利用できるポイントコードはありません</p>
+							<p class="alncenter"><a href="/" class="linkbtn">トップページに戻る</a></p>
+						</div>
+						<!--/コンテンツ-->
+						<div id="footer">
+							<address>
+								Copyright 2015 POINT.COM All Rights Reserved
+							</address>
+						</div>
+					</div>
+					<!--/ページメイン部分-->
+				</div>
+				<!--/メイン全体-->
+			</div>
+			<!--/全体-->
+			<!--スライド-->
+			<?php include_once dirname(__FILE__).'/../../common/slide_contents.php';?>
+			<!-- /スライド-->
+		</body>
+</html>
---
+++ mypage/point_code/detail.php
@@ -0,0 +1,5 @@
+<?php
+include dirname(__FILE__).'/../../include/mypage/Point_codePage.php';
+
+$obj = new Point_codePage();
+$obj->detailAction();
