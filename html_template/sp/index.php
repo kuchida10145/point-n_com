33d906ac7482658c34bbf10eb3160024c2e3e738
丹下 和学 <tange@6web.co.jp>
2015-06-15 18:38:36 +0900

    フロント：店舗検索：近くの店舗機能の追加分のプログラムが一部消失している(refs #387)

--- html_template/sp/index.php
+++ html_template/sp/index.php
@@ -25,6 +25,8 @@
 
 </div>
 
+<div id="demo"></div>
+
 
 <!--メイン全体-->
 <div id="mainbodywrap">
@@ -36,6 +38,7 @@
 <input type="hidden" name="category_midium_id" id="category_midium_id" value="<?php echo getParam($post, 'category_midium_id'); ?>"/>
 <input type="hidden" name="category_small_ids" id="category_small_ids" value="<?php echo getParam($post, 'category_small_ids'); ?>"/>
 <input type="hidden" name="area_key_ids" id="area_key_ids" value="<?php echo getParam($post, 'area_key_ids'); ?>"/>
+
 
 <div class="genreselect">
 <h2><span class="icon-shop"></span>ジャンルを選ぶ<?php echo getParam($error, 'category_large_id'); ?></h2>
@@ -56,7 +59,7 @@
 <div class="selectbtn">
 <ul class="clearfix fixHeight">
 	<li class="gpsbtn">
-      <label for="a"><a href="store/gps_list.html">現在地<br />から探す</a></label>
+      <label for="a"><a href="javascript:void(0);" onclick="document.gps.submit();">現在地<br />から探す</a></label>
     </li>
     <?php foreach (region_master() as $val_key => $val_name) : ?>
     <li>
@@ -72,6 +75,14 @@
 	<p><a href="javascript:void(0);" onclick="document.frm.submit();"><span class="searchlinkicon"></span>「店舗」を検索</a></p>
 </div>
 </form>
+
+<!-- 近くのお店を探す機能-->
+<form method="post" name="gps" action="<?php echo HTTP_HOST; ?>/stores/near.php/?tkn=<?php echo getGet('tkn'); ?>">
+    <input type="hidden" name="gps_lat" id="latitude" value=""/>
+    <input type="hidden" name="gps_long" id="longitude" value=""/>
+    <input type="hidden" name="gps_error" id="gps-error" value=""/>
+</form>
+<!-- /近くのお店を探す機能-->
 
 </div>
 <!--/コンテンツ-->
@@ -93,6 +104,9 @@ Copyright 2015 POINT.COM All Rights Reserved
 <!--スライド-->
 <?php include_once dirname(__FILE__).'/common/slide_contents.php';?>
 <!--/スライド-->
-
+<script type="text/javascript" src="<?php echo HTTP_HOST; ?>/js/pointcom.js"></script>
+<script type="text/javascript">
+jQuery(document).ready(pointcom.init);
+</script>
 </body>
 </html>
